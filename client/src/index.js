a467d9f1027a84475f3350bb1bc577af235a993a
Syed-Naqvi-Ali <nrcoollove7214@gmail.com>
2024-07-08 00:38:56 +0200

    changed url

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,6 @@ import reactDOM from "react-dom"
 import Container from "./context/Container.js"
 import { HashRouter } from "react-router-dom"
 import axios from "axios"
-axios.defaults.baseURL=process.env.NODE_ENV==="developement"? "http://localhost:8080":"https://online-chess-app.onrender.com"
+axios.defaults.baseURL=process.env.NODE_ENV==="developement"? "http://localhost:8080":"https://online-chess-app.vercel.app"
 
 reactDOM.render(<HashRouter> <Container> <App/></Container></HashRouter>,document.getElementById("root"))
